2993706f1d0da0c9c01c2266b6e4ae25ee3a10f3
Quentin Vansteene <vansteene.quentin@gmail.com>
2023-03-02 18:25:13 +0100

    [fix] panel position and overflows

--- sidepanel.js
+++ sidepanel.js
@@ -2,8 +2,8 @@ module.exports = function(app){
     var Sidepanel = Object.getPrototypeOf(app).Sidepanel = new app.Component("sidepanel");
     //Sidepanel.debug = true;
     Sidepanel.createdAt      = "2.0.0";
-    Sidepanel.lastUpdate     = "2.0.0";
-    Sidepanel.version        = "1";
+    Sidepanel.lastUpdate     = "2.0.2";
+    Sidepanel.version        = "1.0.1";
     // Sidepanel.factoryExclude = true;
     // Sidepanel.loadingMsg     = "This message will display in the console when component will be loaded.";
     // Sidepanel.requires       = [];
