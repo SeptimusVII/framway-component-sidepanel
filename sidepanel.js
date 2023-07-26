69af8baeddf597336b92d17bb5e88e542a5e1a0c
Quentin Vansteene <vansteene.quentin@gmail.com>
2023-07-26 17:23:47 +0200

    clean and update sample file

--- sidepanel.js
+++ sidepanel.js
@@ -2,8 +2,8 @@ module.exports = function(app){
     var Sidepanel = Object.getPrototypeOf(app).Sidepanel = new app.Component("sidepanel");
     //Sidepanel.debug = true;
     Sidepanel.createdAt      = "2.0.0";
-    Sidepanel.lastUpdate     = "2.3.3";
-    Sidepanel.version        = "1.1.0";
+    Sidepanel.lastUpdate     = "2.4.0";
+    Sidepanel.version        = "1.1.1";
     // Sidepanel.factoryExclude = true;
     // Sidepanel.loadingMsg     = "This message will display in the console when component will be loaded.";
     // Sidepanel.requires       = [];
